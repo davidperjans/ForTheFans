f73fae54e484741d78aca0a8a37057624b016cdf
David Perjans <perjansdavid@gmail.com>
2025-05-07 17:10:40 +0200

    structuring the project with folders and db with nuget packages installed

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration config)
+        {
+            // Add your infrastructure services here
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
